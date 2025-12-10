2b33833e8f1795fdc60ac25960b303c9c27205b0
Lucas G. Budniewski <lucasgb73@gmail.com>
2025-12-10 02:43:11 -0300

    Commit inicial, inserindo arquivos de teste e scripts

--- retorna-numero-vogais.js
+++
@@ -1,9 +0,0 @@
-function retornaNumeroVogais(texto){
-    return texto.split('').filter(letra => 'aeiouAEIOU'.includes(letra)).length
-}
-
-
-const retornaNumeroVogaisArrow = (texto) => texto.split('').filter(letra => 'aAeEiIoOuU'.includes(letra)).length
-
-console.log("Tradicional: "+retornaNumeroVogais("Exemplo de texto"))
-console.log("Arrow: "+retornaNumeroVogaisArrow("Exemplo de texto"))
